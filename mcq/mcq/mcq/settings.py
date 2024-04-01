9665d16ece467524404108bf101bb868a1cbbb23
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-01 21:21:37 +0545

    Korean Language Update

--- mcq/mcq/mcq/settings.py
+++ mcq/mcq/mcq/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-+3(*^q)f%%@m&l^z-lfy#g(-v_p8g&s2*38#w@whfb42&l5+ci
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://mcq.gastii.com','https://mcq.gastii.com','mcq.gastii.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -76,7 +76,6 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
--- mcq/mcq/mcq/urls.py
+++ mcq/mcq/mcq/urls.py
@@ -40,7 +40,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('account.urls')),
     path('app/',include('app.urls')),
-    path('dashboard/',include('app2.urls')),
+    path('',include('app2.urls')),
     path('ckeditor/' ,include('ckeditor_uploader.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
